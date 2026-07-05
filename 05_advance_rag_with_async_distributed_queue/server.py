2ed08a12cb20ec53ad3579d90f99389b75f94049
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-07-05 13:33:42 +0000

    Add a get result route to display polling

--- 05_advance_rag_with_async_distributed_queue/server.py
+++ 05_advance_rag_with_async_distributed_queue/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from .queue.connection import q
 from .queue.worker import process_query
 
@@ -14,3 +14,10 @@ def chat(query: str = Query(..., description="chat message")) :
     job = q.enqueue(process_query, query)
 
     return { "status": "queued", "job_id": job.id}
+
+@app.get("/result/{job_id}")
+def get_result(job_id : str = Path(..., description="Job ID")) : 
+    job = q.fetch_job(job_id=job_id)
+    result = job.return_value()
+    
+    return { "result: ", result}
